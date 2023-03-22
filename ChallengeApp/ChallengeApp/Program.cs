d454a70940efd79f4188d5aca080e47abad72fcd
tomkolt <tomkolt@gmail.com>
2023-03-22 15:27:15 +0100

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,19 @@
-﻿string name = "Ewa";
-bool isMale = false;
-int age = 33;
+﻿using System.Diagnostics.Metrics;
 
-if (isMale == false && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (isMale == true && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else 
-{
-    Console.WriteLine("Brak spełniających kryteriów");
-}
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
+for (int i = 0; i < 10; i++)
+{
+   int count = 0;
+
+    foreach (char letter in letters)
+    {
+        if ((int)Char.GetNumericValue(letter) == i)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(i + " => " + count);
+}
