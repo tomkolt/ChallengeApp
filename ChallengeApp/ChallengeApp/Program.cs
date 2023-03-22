fa114e5de0a8de74f0a3746cde39aabdaaa15478
tomkolt <tomkolt@gmail.com>
2023-03-22 13:06:10 +0100

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool isMale = false;
+int age = 33;
+
+if (isMale == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isMale == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else 
+{
+    Console.WriteLine("Brak spełniających kryteriów");
+}
+
